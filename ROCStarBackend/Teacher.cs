6dce8675bb78e7a683bf9ab496b68a3af2f043a0
VZ <131747053+VaZoi@users.noreply.github.com>
2024-02-08 09:26:32 +0100

    made property Firstname, Lastname, email and Birthdate for class teacher

--- ROCStarBackend/Teacher.cs
+++ ROCStarBackend/Teacher.cs
@@ -3,4 +3,12 @@
 internal class Teacher
 {
     public int ID { get; set; }
+
+    public string FirstName { get; set; }
+
+    public string LastName { get; set; }
+
+    public string Email { get; set; }
+
+    public DateOnly BirthDate { get; set; }
 }
