818c33025422ad2e161a241373a2a2d323c2371f
bertdd <bertdd@me.com>
2024-02-27 10:01:29 +0100

    fixed test

--- ROCStarBackend/Group.cs
+++ ROCStarBackend/Group.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace ROCStarBackend;
 
-namespace ROCStarBackend
+public class Group(string id)
 {
-    internal class Group
-    {
-    }
+  public string Id { get; private set; } = id;
 }
--- ROCtest/UnitTest1.cs
+++ ROCtest/UnitTest1.cs
@@ -5,31 +5,34 @@ namespace ROCtest;
 [TestClass]
 public class ROCStarTests
 {
-    [TestMethod]
-    public void TestCreateStudent()
-    {
-        var student = new Student(1, "Bert");
-        Console.WriteLine(student.Name);
-    }
+  [TestMethod]
+  public void TestCreateStudent()
+  {
+    var student = new Student(1, "Bert");
+    Console.WriteLine(student.Name);
+  }
 
-    [TestMethod]
-    public void TestCreateTeacher()
-    {
-        var student = new Teacher(1, "Milan");
-        Console.WriteLine(student.Name);
-    }
+  [TestMethod]
+  public void TestCreateTeacher()
+  {
+    var student = new Teacher(1, "Milan");
+    Console.WriteLine(student.Name);
+  }
 
-    [TestMethod]
-    public void TestCreateClassroom()
-    {
-        var classroom = new Classroom("A319C");
-        Assert.AreEqual(classroom.Id, "A319C");
-        Console.WriteLine(classroom.Id);
-    }
-    [TestMethod]
-    public void TestCreateGroup()
-    {
-        var group = new Group("group1");
-        Console.WriteLine(group.Id);
-    }
+  [TestMethod]
+  public void TestCreateClassroom()
+  {
+    var classroom = new Classroom("A319C");
+    Assert.AreEqual(classroom.Id, "A319C");
+    Console.WriteLine(classroom.Id);
+  }
+
+  [TestMethod]
+  public void TestCreateGroup()
+  {
+    var group = new Group("group1");
+    Console.WriteLine(group.Id);
+    Assert.IsNotNull(group);
+    Assert.AreEqual(group.Id, "group1");
+  }
 }
