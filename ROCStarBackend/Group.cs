2dd6359a42d5bb9f96f128af9d62811d23bd08dc
bertdd <bertdd@me.com>
2024-02-29 12:28:12 +0100

    Added group from Mustapha

---
+++ ROCStarBackend/Group.cs
@@ -0,0 +1,15 @@
+﻿
+namespace ROCStarBackend;
+
+public class Group(string id)
+{
+  public string Id { get; private set; } = id;
+
+  public List<Student> Students { get; private set; } = [];
+
+  public void Add(Student student) => Students.Add(student);
+
+  public void Remove(Student student) => Students.Remove(student);
+
+  public override string ToString() => Id;
+}
