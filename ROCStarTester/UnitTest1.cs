b0d1211b21c486e110282cced0eb03c15f218257
bertdd <bertdd@me.com>
2024-03-14 12:40:08 +0100

    fixed updates

--- ROCStarTester/UnitTest1.cs
+++ ROCStarTester/UnitTest1.cs
@@ -8,14 +8,14 @@ public class UnitTest1
   [TestMethod]
   public void TestCreateStudent()
   {
-    var student = new Student("Mous");
+    var student = new Student(1, "Mous");
     Assert.IsNotNull(student);
   }
 
   [TestMethod]
   public void TestCreateDocent()
   {
-    var docent = new Docent("Bert");
+    var docent = new Docent(13, "Bert");
     Assert.IsNotNull(docent);
     Assert.AreSame(docent.Name, "Bert");
   }
