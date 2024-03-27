23b3fc1e85b6730d4e01a75d3ffdcf628834a188
bertdd <bertdd@me.com>
2024-03-27 11:19:17 +0100

    finished database access

--- ROCStarBackend/RealData.cs
+++ ROCStarBackend/RealData.cs
@@ -1,11 +1,34 @@
-﻿namespace ROCStarBackend;
+﻿using MySql.Data.MySqlClient;
+
+namespace ROCStarBackend;
 
 public class RealData
 {
   public static Group GetGroup()
   {
-    var result = new Group("OITSDO2A");
+    var group = new Group("OITSDO2A");
 
-    return result;
+    var connectionString = new MySqlConnectionStringBuilder
+    {
+      UserID = "root",
+      Password = "",
+      Port = 3306,
+      Server = "localhost",
+      Database = "test"
+    }.ToString();
+
+    using var connection = new MySqlConnection(connectionString);
+    connection.Open();
+
+    using var command = connection.CreateCommand();
+    command.CommandText = "select * from student";
+    using var reader = command.ExecuteReader();
+    while (reader.Read())
+    {
+      group.Students.Add(new Student((int)reader["id"], (string)reader["name"]));
+    }
+
+    connection.Close();
+    return group;
   }
 }
